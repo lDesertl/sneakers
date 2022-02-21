a5264ba5371fddd9a00f18823ea414a041e8ddf1
Timur <rebeldesert@yandex.ru>
2022-02-22 01:42:16 +0500

    +cart, red slider

--- src/App.js
+++ src/App.js
@@ -58,8 +58,25 @@ function App() {
                     </ul>
                 </div>
             </header>
-            <slider className="d-flex justify-center">
-                <img src="img/slider.png" alt="slider"/>
+            <slider className="d-flex justify-center slider">
+                <div className="sliderWrap d-flex justify-center">
+                <div className="arrows">
+                <div className="SliderRightArrow d-flex align-center justify-center">
+                    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 0.999999L6 6L1 11" stroke="#C8C8C8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                </div>
+
+                <div className="SliderLeftArrow d-flex align-center justify-center">
+                    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 0.999999L6 6L1 11" stroke="#C8C8C8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                </div>
+                </div>
+
+                </div>
             </slider>
             <div className="content p-40">
                 <h1>
@@ -490,6 +507,28 @@ function App() {
                 </div>
 
             </div>
+            <cart>
+                <div className="blackout">
+                    <div></div>
+                </div>
+                <div className="cart">
+                    <h2 className="H2">Корзина</h2>
+                    <div>
+                        <div className="cartWrapper d-flex flex-column justify-between align-center text-center">
+                        <img src="./img/box.png" width={120} height={120} alt=""/>
+                        <h2>Корзина пустая</h2>
+
+                        <h4>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</h4>
+                        <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                            &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            </cart>
         </div>);
 
 }
