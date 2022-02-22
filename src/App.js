9f4be3b1b0295b27d3f1abcaae53737f66cc233c
Timur <rebeldesert@yandex.ru>
2022-02-22 17:23:09 +0500

    +cart, red slider

--- src/App.js
+++ src/App.js
@@ -507,7 +507,51 @@ function App() {
                 </div>
 
             </div>
-            <cart>
+            {/*<emptycart>*/}
+            {/*    <div className="blackout">*/}
+            {/*        <div></div>*/}
+            {/*    </div>*/}
+            {/*    <div className="cart">*/}
+            {/*        <h2 className="H2">Корзина</h2>*/}
+            {/*        <div>*/}
+            {/*            <div className="cartWrapper d-flex flex-column justify-between align-center text-center">*/}
+            {/*            <img src="./img/box.png" width={120} height={120} alt=""/>*/}
+            {/*            <h2>Корзина пустая</h2>*/}
+
+            {/*            <h4>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</h4>*/}
+            {/*            <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
+            {/*                <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
+            {/*                <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
+            {/*            </svg>*/}
+            {/*                &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад*/}
+            {/*            </button>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</emptycart>*/}
+            {/*<ordercart>*/}
+            {/*    <div className="blackout">*/}
+            {/*        <div></div>*/}
+            {/*    </div>*/}
+            {/*    <div className="cart">*/}
+            {/*        <h2 className="H2">Корзина</h2>*/}
+            {/*        <div>*/}
+            {/*            <div className="cartWrapper d-flex flex-column justify-between align-center text-center">*/}
+            {/*                <img src="./img/orderlist.jpg" width={83} height={120} alt=""/>*/}
+            {/*                <div className="green"><h2>Заказ Оформлен!</h2></div>*/}
+
+            {/*                <h4>Ваш заказ #18 скоро будет передан курьерской доставке.</h4>*/}
+            {/*                <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
+            {/*                    <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
+            {/*                    <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
+            {/*                </svg>*/}
+            {/*                    &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад*/}
+            {/*                </button>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</ordercart>*/}
+            <fullcart>
                 <div className="blackout">
                     <div></div>
                 </div>
@@ -515,20 +559,31 @@ function App() {
                     <h2 className="H2">Корзина</h2>
                     <div>
                         <div className="cartWrapper d-flex flex-column justify-between align-center text-center">
-                        <img src="./img/box.png" width={120} height={120} alt=""/>
-                        <h2>Корзина пустая</h2>
-
-                        <h4>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</h4>
-                        <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                            &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад
-                        </button>
+                            <div className="cartcardswrapper d-flex flex-column">
+                                <div className="cartcards d-flex align-center justify-center flex-row">
+                                <img src="./img/sneakers/1.jpg" width={70} height={70} alt=""/>
+                                <div className="d-flex flex-column coastDiscription">
+                                    <div className="discrpition">
+                                        <h5> Мужские Кроссовки Nike Air Max 270</h5>
+                                    </div>
+                                    <div className="coast">
+                                        <h5>
+                                        12 999 руб.
+                                        </h5>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                                &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад
+                            </button>
                         </div>
                     </div>
                 </div>
-            </cart>
+            </fullcart>
         </div>);
 
 }
