c39647d3b43b47bc686bc44568c069d98ab7d24a
Timur <rebeldesert@yandex.ru>
2022-02-25 02:32:31 +0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -314,120 +314,120 @@ function App() {
                 </div>
 
             </div>
-            {/*<emptycart>*/}
-            {/*    <div className="blackout">*/}
-            {/*        <div></div>*/}
-            {/*    </div>*/}
-            {/*    <div className="cart">*/}
-            {/*        <h2 className="H2">Корзина</h2>*/}
-            {/*        <div>*/}
-            {/*            <div className="cartWrapper d-flex flex-column justify-between align-center text-center">*/}
-            {/*            <img src="./img/box.png" width={120} height={120} alt=""/>*/}
-            {/*            <h2>Корзина пустая</h2>*/}
+            <emptycart>
+                <div className="blackout">
+                    <div></div>
+                </div>
+                <div className="cart">
+                    <h2 className="H2">Корзина</h2>
+                    <div>
+                        <div className="cartWrapper d-flex flex-column justify-between align-center text-center">
+                        <img src="./img/box.png" width={120} height={120} alt=""/>
+                        <h2>Корзина пустая</h2>
 
-            {/*            <h4>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</h4>*/}
-            {/*            <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-            {/*                <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
-            {/*                <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
-            {/*            </svg>*/}
-            {/*                &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад*/}
-            {/*            </button>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</emptycart>*/}
-            {/*<ordercart>*/}
-            {/*    <div className="blackout">*/}
-            {/*        <div></div>*/}
-            {/*    </div>*/}
-            {/*    <div className="cart">*/}
-            {/*        <h2 className="H2">Корзина</h2>*/}
-            {/*        <div>*/}
-            {/*            <div className="cartWrapper d-flex flex-column justify-between align-center text-center">*/}
-            {/*                <img src="./img/orderlist.jpg" width={83} height={120} alt=""/>*/}
-            {/*                <div className="green"><h2>Заказ Оформлен!</h2></div>*/}
+                        <h4>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</h4>
+                        <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                            &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            </emptycart>
+            <ordercart>
+                <div className="blackout">
+                    <div></div>
+                </div>
+                <div className="cart">
+                    <h2 className="H2">Корзина</h2>
+                    <div>
+                        <div className="cartWrapper d-flex flex-column justify-between align-center text-center">
+                            <img src="./img/orderlist.jpg" width={83} height={120} alt=""/>
+                            <div className="green"><h2>Заказ Оформлен!</h2></div>
 
-            {/*                <h4>Ваш заказ #18 скоро будет передан курьерской доставке.</h4>*/}
-            {/*                <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-            {/*                    <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
-            {/*                    <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
-            {/*                </svg>*/}
-            {/*                    &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад*/}
-            {/*                </button>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</ordercart>*/}
-            {/*<fullcart>*/}
-            {/*    <div className="blackout">*/}
+                            <h4>Ваш заказ #18 скоро будет передан курьерской доставке.</h4>
+                            <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                                &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </ordercart>
+            <fullcart>
+                <div className="blackout">
 
-            {/*    </div>*/}
-            {/*    <div className="cart">*/}
-            {/*        <h2 className="H2">Корзина</h2>*/}
-            {/*        <div>*/}
-            {/*            <div className="cartWrapper d-flex flex-column justify-between align-center text-center">*/}
-            {/*                <div className="cartcardswrapper d-flex flex-column">*/}
-            {/*                    {dataSneakers.map(({img, id, price, name, description, like, cart}) => {*/}
+                </div>
+                <div className="cart">
+                    <h2 className="H2">Корзина</h2>
+                    <div>
+                        <div className="cartWrapper d-flex flex-column justify-between align-center text-center">
+                            <div className="cartcardswrapper d-flex flex-column">
+                                {dataSneakers.map(({img, id, price, name, description, like, cart}) => {
 
-            {/*                            return (*/}
+                                        return (
 
-            {/*                                <div>*/}
-            {/*                                    {cart ?*/}
-            {/*                                        <div className="cartcards d-flex align-center justify-center flex-row">*/}
-            {/*                                            <img src={img} width={70} height={70} alt=""/>*/}
-            {/*                                            <div className="d-flex flex-column justify-center coastDescription">*/}
-            {/*                                                <div className="description">*/}
-            {/*                                                    <h5> {description + " " + name}</h5>*/}
-
-
-            {/*                                                    <h5 className="coast">*/}
-            {/*                                                        {price + " руб."}*/}
-            {/*                                                    </h5>*/}
-            {/*                                                </div>*/}
-            {/*                                            </div>*/}
-            {/*                                        </div>*/}
-
-            {/*                                        :*/}
-            {/*                                        ""*/}
-
-            {/*                                    }*/}
-            {/*                                </div>*/}
-
-            {/*                            )*/}
+                                            <div>
+                                                {cart ?
+                                                    <div className="cartcards d-flex align-center justify-center flex-row">
+                                                        <img src={img} width={70} height={70} alt=""/>
+                                                        <div className="d-flex flex-column justify-center coastDescription">
+                                                            <div className="description">
+                                                                <h5> {description + " " + name}</h5>
 
 
-            {/*                        }*/}
-            {/*                    )*/}
-            {/*                    }*/}
-            {/*                </div>*/}
-            {/*                <button className="orderbtn d-flex  align-center justify-center">*/}
+                                                                <h5 className="coast">
+                                                                    {price + " руб."}
+                                                                </h5>
+                                                            </div>
+                                                        </div>
+                                                    </div>
 
-            {/*                    Оформить заказ&nbsp;&nbsp;&nbsp;&nbsp;*/}
-            {/*                    <svg width="16" height="14" viewBox="0 0 16 14" fill="none"*/}
-            {/*                                                               xmlns="http://www.w3.org/2000/svg">*/}
-            {/*                    <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2"*/}
-            {/*                          stroke-linecap="round" stroke-linejoin="round"/>*/}
-            {/*                    <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round"*/}
-            {/*                          stroke-linejoin="round"/>*/}
-            {/*                </svg>*/}
-            {/*                </button>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</fullcart>*/}
-            {/*<div className="likecartempty">*/}
-            {/*    <div className="none">*/}
-            {/*        <img src="img/smile1.png" width={70} height={70} alt=""/>*/}
-            {/*        <h2>Закладок нет :(</h2>*/}
-            {/*        <h3>Вы ничего не добавляли в закладки</h3>*/}
-            {/*                    <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-            {/*                            <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
-            {/*                        <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>*/}
-            {/*                    </svg>*/}
-            {/*                        &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад*/}
-            {/*                    </button>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+                                                    :
+                                                    ""
+
+                                                }
+                                            </div>
+
+                                        )
+
+
+                                    }
+                                )
+                                }
+                            </div>
+                            <button className="orderbtn d-flex  align-center justify-center">
+
+                                Оформить заказ&nbsp;&nbsp;&nbsp;&nbsp;
+                                <svg width="16" height="14" viewBox="0 0 16 14" fill="none"
+                                                                           xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2"
+                                      stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round"
+                                      stroke-linejoin="round"/>
+                            </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </fullcart>
+            <div className="likecartempty">
+                <div className="none">
+                    <img src="img/smile1.png" width={70} height={70} alt=""/>
+                    <h2>Закладок нет :(</h2>
+                    <h3>Вы ничего не добавляли в закладки</h3>
+                                <button className="backbtn d-flex  align-center justify-center"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M14.7144 7L1.00007 7" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;Вернуться назад
+                                </button>
+                </div>
+            </div>
             <div className="likecart flex-column">
                 <div className="d-flex align-center">
 
