46139c913d300cb5f80c74ce107aa1608f725aae
Timur <rebeldesert@yandex.ru>
2022-03-01 21:40:00 +0500

    trying to connect the swiper

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,50 @@
 import React from 'react';
+// core version + navigation, pagination modules:
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// import styles bundle
+import 'swiper/css/bundle';
+import Swiper from 'swiper/bundle';
+import {Navigation, Pagination, Scrollbar} from "swiper";
+
+
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination, Scrollbar],
+
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
 
 const Slider = () => {
+
+
     return (
+
         <div>
+
+            <div className="swiper">
+
+                <div className="swiper-wrapper">
+
+                    <div className="swiper-slide d-flex align-center justify-center"><img src="img/slider.png" alt=""/></div>
+                    <div className="swiper-slide">Slide 2</div>
+                    <div className="swiper-slide">Slide 3</div>
+                </div>
+
+
+                <div className="swiper-button-prev"></div>
+                <div className="swiper-button-next"></div>
+            </div>
             <slider className="d-flex justify-center slider">
                 <div className="sliderWrap d-flex justify-center">
                     <div className="arrows">
