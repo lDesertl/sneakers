0c0715220bf22d99c8544713924e9d109da16162
Timur <rebeldesert@yandex.ru>
2022-03-01 21:57:35 +0500

    +swiper

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -32,41 +32,18 @@ const Slider = () => {
 
         <div>
 
-            <div className="swiper">
 
-                <div className="swiper-wrapper">
-
-                    <div className="swiper-slide d-flex align-center justify-center"><img src="img/slider.png" alt=""/></div>
-                    <div className="swiper-slide">Slide 2</div>
-                    <div className="swiper-slide">Slide 3</div>
-                </div>
-
-
-                <div className="swiper-button-prev"></div>
-                <div className="swiper-button-next"></div>
-            </div>
             <slider className="d-flex justify-center slider">
-                <div className="sliderWrap d-flex justify-center">
-                    <div className="arrows">
-                        <div className="SliderRightArrow d-flex align-center justify-center">
-                            <svg width="7" height="12" viewBox="0 0 7 12" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 0.999999L6 6L1 11" stroke="#C8C8C8" stroke-width="1.5"
-                                      stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
+                <div className="swiper">
 
-                        </div>
+                    <div className="swiper-wrapper">
 
-                        <div className="SliderLeftArrow d-flex align-center justify-center">
-                            <svg width="7" height="12" viewBox="0 0 7 12" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 0.999999L6 6L1 11" stroke="#C8C8C8" stroke-width="1.5"
-                                      stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-
-                        </div>
+                        <div className="swiper-slide d-flex align-center justify-center"><img src="img/slider.png" alt=""/></div>
+                        <div className="swiper-slide d-flex align-center justify-center"><img src="img/froggy.png" alt=""/></div>
+                        <div className="swiper-slide d-flex align-center justify-center"><img src="img/sneakers/1.jpg" alt=""/></div>
                     </div>
-
+                    <div className="swiper-button-prev"></div>
+                    <div className="swiper-button-next"></div>
                 </div>
             </slider>
         </div>
